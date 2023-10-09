a9131d6e487700ad960e6b216cef035658b716d7
Justine Clément-Savary <clement.savary.justine@gmail.com>
2023-10-09 11:15:57 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+require('dotenv').config() 
+
+
+console.log(`The server will run on port ${process.env.SERVER_PORT}`);
+console.log(`I am ${process.env.MY_NAME}, wilder in ${process.env.MY_CITY}, and I love ${process.env.MY_LANGUAGE}`);
+
